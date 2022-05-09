5cc79d3010e0e35b238a21d454e942c6924769c6
PereEscoda <105195578+PereEscoda@users.noreply.github.com>
2022-05-09 20:17:50 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -1,51 +1,39 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include "picross.h"
-#include <stdbool.h>
+
 
 int main()
 {
-    fit_t c;
-    if(obrir_fitxer(&c))
+    FILE * fit;
+    fit = fopen ( "input.txt", "r");
+    int i, j, x, y, taula[10][10], taula1[10][10], comptador, compta2;
+    int max_f, max_c, max_errors;
+    if (fit== NULL)
     {
-        //mostrar(&c);
+        printf("Error\n");
     }
     else
     {
+        fscanf(fit,  "%d", &max_f);
+        fscanf(fit,  "%d", &max_c);
+        fscanf(fit,  "%d", &max_errors);
 
-        printf("AI CARAMBA!");
-    }
-/*
-            comptador=0;
-            x=0;  y=0;
-            printf("   ");
-            for (i=0; i<max_f+1; i++)
-            {
-               for (j=0; j< max_c+1; j++)
-                {
-                    if(taula[i][j]==1)
-                    {
-                        comptador++;
-                    }
-                    else
-                    {
-                        if (comptador!=0)
-                        {
-                            taula1[x][y]= comptador;
-                            y++;
-                            printf ("%d", comptador);
-                            comptador=0;
-
-                        }
-                    }
-                }
-                printf(" ");
-                x++;
-            }
+               for (i=0; i< max_f; i++)
+               {
+                   for (j=0; j< max_c; j++)
+                   {
+                       fscanf(fit, "%d", &taula[i][j]);
+                       printf("%d ", taula[i][j]);
+                   }
+                   printf("\n");
+               }
+//
             printf("\n");
-             comptador=0;
-             x=0;  y=0;
-             for (j=0; j< max_c+1; j++)
+            comptador=0;
+            compta2=0;
+            x=0; y=1; /*freestlye*/
+            printf("   ");
+            for (j=0; j< max_c; j++)
             {
                 for (i=0; i<max_f+1; i++)
                 {
@@ -57,22 +45,71 @@ int main()
                     {
                         if (comptador!=0)
                         {
-                            taula1[x][y]= comptador;
-                            x++;
-                            printf ("%d", comptador);
+                            compta2 = compta2*10+comptador;
                             comptador=0;
-
                         }
-
-
                     }
                 }
+                printf("%d", compta2);
+                taula1[x][y]=compta2; /*freestlye*/
+                y++; /*freestlye*/
+                printf(" ");
+                compta2=0;
+                comptador=0;
+            }
+
+            comptador=0;
+            compta2=0;
+            x=1; y=0; /*freestlye*/
+            printf("\n");
+            for (i=0; i<max_f; i++)
+            {
+               for (j=0; j< max_c+1; j++)
+                {
+                    if(taula[i][j]==1)
+                    {
+                        comptador++;
+                    }
+                    else
+                    {
+                        if (comptador!=0)
+                        {
+                            compta2 = compta2*10+comptador;
+                            comptador=0;
+                        }
+                    }
+                }
+                printf("%d", compta2);
+                taula1[x][y]=compta2; /*freestlye*/
+                x++; /*freestlye*/
                 printf("\n");
-                y++;
+                compta2=0;
+                comptador=0;
+
+            }
+
+            /*a partir d'aquí és freestyle*/
+            printf("\n\n");
+            x=1; y=1;
+            for (i=0; i<max_f; i++)
+            {
+               for (j=0; j< max_c; j++)
+               {
+                   taula1[x][y]=taula[i][j];
+                   y++;
+               }
+               x++;
+            }
+
+
+            for (x=0; x<max_f; x++)
+            {
+               for (y=0; y< max_c; y++)
+               {
+                   printf("%d", taula1[x][y]);
+               }
             }
     }
 
-
-*/
     return 0;
 }
